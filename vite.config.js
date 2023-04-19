40c7d848089a1e87dbae9f7355f7f012badab253
Thierry0 <thierrymi@yahoo.fr>
2023-04-19 09:31:33 -0400

    Added PWA and minor branding changes

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,56 @@ export default defineConfig({
         replacement: './runtimeConfig.browser',
       },
     ]
+  },
+  pwa: {
+    name: 'TM App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      icons: [
+        {
+          "src": "./assets/icons/tm-icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/icons/tm-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
+    },
   }
 })
